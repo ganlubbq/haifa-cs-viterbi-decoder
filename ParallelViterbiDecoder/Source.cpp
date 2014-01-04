f877cd3536148134112439c8e27b9ee2ccf973f1
shiranbm <shiranbm@gmail.com>
2014-01-04 21:20:50 +0000

    updated encoding

--- ParallelViterbiDecoder/Source.cpp
+++ ParallelViterbiDecoder/Source.cpp
@@ -7,66 +7,104 @@ using namespace std;
 
 class Viterbi {
 	int inputBits, outputBits, registersCount, constrainLength, parallelism;
-	vector< vector<bool> > polynomials;
-	vector< vector<bool> > memoryRegisters;	
+	vector< vector<bool> > xors;
+	vector<bool> memoryRegisters;
+	vector<bool> encodedData;
 
 public:
 	// Constructer - will create the encoder and matching decoder
 	Viterbi (int n, int k, int m, int p) {
-		inputBits = k;
 		outputBits = n;
+		inputBits = k;
 		registersCount = m;
 		parallelism = p;
 
 		// calculate constrain length
-		constrainLength = inputBits * (registersCount - 1);
+		constrainLength = inputBits * registersCount;
 
 		// initilize polynomials with random sets of bits to xor
-		polynomials.resize(outputBits);
+		xors.resize(outputBits);
+		srand(time(NULL));
 		for (int i = 0; i < outputBits; i++)
 		{
-			polynomials.resize(inputBits * registersCount);
-			for (int bit = 0; bit < inputBits * registersCount; bit++)
+			xors[i].resize(constrainLength);
+			for (int bit = 0; bit < constrainLength; bit++)
 			{
-				polynomials[i][bit] = (rand()%2 != 0);
+				xors[i][bit] = (rand()%2 != 0);
 			}
+			cout << "xor" << i << ": ";
+			PrintVectorBool(xors[i]);
 		}
 
+		memoryRegisters = vector<bool>(constrainLength , 0);
 		// inilize the registers each with inputBits number of bits
-		memoryRegisters.resize(registersCount);
-		for (int j = 0; j < registersCount; j++)
+		memoryRegisters.resize(constrainLength);
+		for (int j = 0; j < constrainLength; j++)
 		{
-			memoryRegisters[j].resize(inputBits);
+			memoryRegisters[j] = 0;
 		}
 	}
 
+	void PerformXors()
+	{
+		for (int i = 0; i < outputBits; i++)
+		{
+			int bit = 0;
+			// Find the first bit in memory to include in the xor
+			try 
+			{
+				while (xors[i][bit] != 1)
+				{
+					bit++;
+					if (bit == constrainLength) throw;
+				}
+			}
+			catch (exception) 
+			{
+				cout << "BAD XORS (all zeros).\n";
+			}
+
+			bool dataOut = memoryRegisters[bit];
+
+			for (int j = bit + 1; j < constrainLength - 1; j++)
+			{
+				// If bit in xor is on perform xor with previous data
+				if (xors[i][j]) dataOut ^= memoryRegisters[j];
+			}
+			encodedData.push_back(dataOut);
+		}
+	}
+
+	void PrintVectorBool(vector<bool> data)
+	{
+		for (int i = 0; i < data.size(); i++)
+		{
+			char bit = (data[i])? '1' : '0';
+			cout << bit;
+		}
+		cout << "\n";
+	}
+	
 	void ShiftRight(bool input)
 	{
-		for (int i = 0; i < (inputBits * registersCount); i++)
+		int i;
+		for (i = memoryRegisters.size()-1; i > 0 ; i--)
 		{
-			memoryRegisters[i+1] = memoryRegisters[i];
+			memoryRegisters[i] = memoryRegisters[i-1];
 		}
+		memoryRegisters[i] = input;
 	}
 
-	vector<bool> Encode(vector<bool> data) 
+	void Encode(vector<bool> data) 
 	{
-		vector<bool> result;
-		result.resize(sizeof(data)*outputBits);
-
-		for (int i = 0; i < sizeof(data); i++)
+		for (int i = 0; i < data.size(); i = i + inputBits)
 		{
-			ShiftRight(data[i]);
-			bool temp;
-			for (int j = 0; j < outputBits; j++)
-			{
-				for (int bit = 0; bit < inputBits * registersCount; bit++)
-				{
-					if (polynomials[i][bit]) temp = temp ^ memoryRegisters[i][bit];
-				}
-				result[i+j] = temp;
-			}
+			for (int j = 0; j < inputBits; j++)
+				ShiftRight(data[i + j]);
+			PerformXors();
 		}
-		return result;
+		cout << "Encoded Data: ";
+		PrintVectorBool(encodedData);
 	}
 
 	void GenerateAutomata()
@@ -74,9 +112,15 @@ public:
 
 	}
 
-	void Send()
+	void Send(vector<bool> data)
 	{
-		// do stuff...
+		Encode(data);
+		Scramble();
+	}
+
+	void Scramble()
+	{
+
 	}
 
 	void Decode()
@@ -88,9 +132,14 @@ public:
 int main() 
 {
 	cout << "Create Viterbi Module\n";
-	Viterbi viterbiTester = Viterbi(2, 1, 2, 0);
-
-	cout << "foo and bar completed.\n";
+	Viterbi viterbiTester = Viterbi(3, 1, 3, 0);
+	
+	bool data[] = {0, 0, 1, 1};
+	std::vector<bool> input (data, data + sizeof(data) / sizeof(bool) );
+	cout << "Input Data: ";
+	viterbiTester.PrintVectorBool(input);
+	
+	viterbiTester.Send(input);
 
 	return 0;
 }
