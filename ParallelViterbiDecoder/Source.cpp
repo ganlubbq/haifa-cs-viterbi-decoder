25ee599f2c45517bcdb7435b6db361b4355bd36a
shiranbm <shiranbm@gmail.com>
2014-01-05 21:48:03 +0000

    Change all implementation to use uint32_t

--- ParallelViterbiDecoder/Source.cpp
+++ ParallelViterbiDecoder/Source.cpp
@@ -3,13 +3,22 @@
 #include <thread>         // std::thread
 #include <bitset>         // std::bitset
 #include <vector>		  // std::vector
+#include <map>			  // std:map
+#include <string>		  // std:string
 using namespace std;
+
+struct DictionaryValue {
+	uint32_t state;
+	uint32_t output;
+}typedef state;
 
 class Viterbi {
 	int inputBits, outputBits, registersCount, constrainLength, parallelism;
-	vector< vector<bool> > xors;
-	vector<bool> memoryRegisters;
-	vector<bool> encodedData;
+	vector<uint32_t> xors;
+	uint32_t memoryRegisters;
+	vector<uint32_t> encodedData;
+	vector<uint32_t> scrambledData;
+	map<uint32_t, vector<state>> automata;
 
 public:
 	// Constructer - will create the encoder and matching decoder
@@ -27,119 +36,179 @@ public:
 		srand(time(NULL));
 		for (int i = 0; i < outputBits; i++)
 		{
-			xors[i].resize(constrainLength);
-			for (int bit = 0; bit < constrainLength; bit++)
-			{
-				xors[i][bit] = (rand()%2 != 0);
-			}
-			cout << "xor" << i << ": ";
-			PrintVectorBool(xors[i]);
+			uint32_t numOfBits = pow(2, constrainLength);
+			xors[i] = rand() % numOfBits;
+			cout << "XOR" << i << ": ";
+			PrintBitSet(xors[i], constrainLength);
+			cout << "\n";
 		}
 
-		memoryRegisters = vector<bool>(constrainLength , 0);
-		// inilize the registers each with inputBits number of bits
-		memoryRegisters.resize(constrainLength);
-		for (int j = 0; j < constrainLength; j++)
-		{
-			memoryRegisters[j] = 0;
-		}
+		// init memoryRegisters
+		memoryRegisters = 0;
+
+		// Create the State Machine to be used later in decode
+		GenerateAutomata();
 	}
 
-	void PerformXors()
+	void Encode(char *data)
 	{
+		// Init pointer to iterate on input data
+		char *dataPtr = data;
+
+		// Move over char input with jumps of size inputbits
+		for (int i = 0; i < strlen(data); i = i + inputBits)
+		{
+			// Take the next Block to encode with size of InputBits
+			char* block = new char[inputBits]();
+			strncpy_s(block, sizeof(block), dataPtr, inputBits);
+			// Shift Right the bits within the block
+			for (int j = inputBits - 1; j >= 0; j--) 
+			{
+				ShiftRight((block[j] == '1')? 1 : 0);
+			}
+			// Calculate the output using XORS * outputBits and Push the encoded result to encoded output
+			encodedData.push_back(CalcOutput(memoryRegisters));
+			// Move to start of next Block
+			dataPtr += inputBits;
+		}
+
+		// Print out the encoded data in blocks (each for one input symbol)
+		cout << "Encoded Data:\n";
+		for (int i = 0; i < encodedData.size(); i++)
+		{
+			PrintBitSet(encodedData[i], constrainLength);
+		}
+		cout << "\n";
+	}
+
+	void ShiftRight(bool input)
+	{
+		// Each time shift right by 1 bit and put the next input bit on left (reading wise)
+		// We always use bitset of 32 (assumed to be max of constrain length) but we only refer to constrain length
+		memoryRegisters = (memoryRegisters >> 1) | (input << constrainLength - 1);
+	}
+
+	uint32_t CalcOutput(uint32_t input)
+	{
+		// Init temp values for calculation
+		bool currXOR;
+		bool first = true;
+		uint32_t output = 0;
+		bitset<32> buffer = bitset<32>(input);
+
+		// Perform XORS as many as outputBits
 		for (int i = 0; i < outputBits; i++)
 		{
-			int bit = 0;
-			// Find the first bit in memory to include in the xor
-			try 
+			// Get the current xor
+			bitset<32> xor = bitset<32>(xors[i]);
+			first = true;
+
+			// Perform XOR between all bits in the buffer which has 1 in relative position on the xors vector
+			for (int bit = 0; bit < constrainLength; bit++)
 			{
-				while (xors[i][bit] != 1)
+				if ((xor[bit] == 1) && first)
 				{
-					bit++;
-					if (bit == constrainLength) throw;
+					currXOR = buffer[bit];
+					first = false;
 				}
-			}
-			catch (exception) 
-			{
-				cout << "BAD XORS (all zeros).\n";
+				else if (xor[bit] == 1) 
+					currXOR ^= buffer[bit];
 			}
 
-			bool dataOut = memoryRegisters[bit];
-
-			for (int j = bit + 1; j < constrainLength - 1; j++)
-			{
-				// If bit in xor is on perform xor with previous data
-				if (xors[i][j]) dataOut ^= memoryRegisters[j];
-			}
-			encodedData.push_back(dataOut);
+			// Put the result in output in consistent order (left to right)
+			output = (output << 1) | currXOR;
 		}
+		return output;
 	}
 
-	void PrintVectorBool(vector<bool> data)
+	void PrintBitSet(bitset<32> data, int count)
 	{
-		for (int i = 0; i < data.size(); i++)
+		// Print bitset from left to right (for clearer reading)- only the actual part used
+		for (int i = count - 1; i >= 0; i--)
 		{
 			char bit = (data[i])? '1' : '0';
 			cout << bit;
 		}
-		cout << "\n";
-	}
-	
-	void ShiftRight(bool input)
-	{
-		int i;
-		for (i = memoryRegisters.size()-1; i > 0 ; i--)
-		{
-			memoryRegisters[i] = memoryRegisters[i-1];
-		}
-		memoryRegisters[i] = input;
-	}
-
-	void Encode(vector<bool> data) 
-	{
-		for (int i = 0; i < data.size(); i = i + inputBits)
-		{
-			for (int j = 0; j < inputBits; j++)
-				ShiftRight(data[i + j]);
-			PerformXors();
-		}
-		cout << "Encoded Data: ";
-		PrintVectorBool(encodedData);
 	}
 
 	void GenerateAutomata()
 	{
-
+		// Create states as many as 2^constrainLength
+		for (uint32_t newState = 0; newState < pow(2, constrainLength); newState++)
+		{
+			// Temp vector to contain the valid states we can get from current state
+			vector<state> tempVector;
+			for (int j = 0; j < pow(2, inputBits); j++)
+			{
+				// for each input bit possible create new state with output
+				state tempState;
+				tempState.state = (newState >> inputBits) | (j << constrainLength - 1);
+				tempState.output = CalcOutput(tempState.state);
+				tempVector.push_back(tempState);
+			}
+			automata[newState] = tempVector;
+		}
 	}
 
-	void Send(vector<bool> data)
+	void PrintAutomata()
 	{
+		cout << "Automata:\n";
+		for(std::map<uint32_t, vector<state>>::iterator iter = automata.begin(); iter != automata.end(); ++iter)
+		{
+			PrintBitSet(bitset<32>(iter->first), constrainLength);
+			cout << " -> \t";
+			for (int input = 0; input < iter->second.size(); input++)
+			{
+				PrintBitSet(bitset<32>(input), inputBits);
+				cout << ": ";
+				PrintBitSet(bitset<32>(iter->second[input].state), constrainLength);
+				cout << "/";
+				PrintBitSet(bitset<32>(iter->second[input].output), constrainLength);
+				cout << "\t";
+			}
+			cout << "\n";
+		}
+		cout << "\n";
+	}
+	void Send(char* data)
+	{
+		// First encode the data
 		Encode(data);
+		// Filp some bits to simulate bus interference
 		Scramble();
 	}
 
 	void Scramble()
 	{
-
+		scrambledData = encodedData;
+		for (int i = 0; i < outputBits; i++)
+		{
+			//scrambledData[rand() % scrambledData.size()].flip();
+		}
+		cout << "Scarmbled Data:\n";
+		for (int i = 0; i < scrambledData.size(); i++)
+		{
+			PrintBitSet(scrambledData[i], constrainLength);
+		}
+		cout << "\n";
 	}
 
 	void Decode()
 	{
-		// do stuff...
+		// Decode data from bus == scrambledData
 	}
 };
 
 int main() 
 {
 	cout << "Create Viterbi Module\n";
-	Viterbi viterbiTester = Viterbi(3, 1, 3, 0);
-	
-	bool data[] = {0, 0, 1, 1};
-	std::vector<bool> input (data, data + sizeof(data) / sizeof(bool) );
-	cout << "Input Data: ";
-	viterbiTester.PrintVectorBool(input);
-	
+	Viterbi viterbiTester = Viterbi(3, 2, 3, 0);
+
+	char *input = "011011";
+	cout << "Input Data:\n" << input << "\n";
+
 	viterbiTester.Send(input);
+	viterbiTester.PrintAutomata();
 
 	return 0;
 }
